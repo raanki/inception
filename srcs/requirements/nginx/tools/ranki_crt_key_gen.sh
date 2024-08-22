b31b5eff799f85297d95ac08995321c112f6db96
raanki <rabalone94@gmail.com>
2024-08-22 23:32:24 +0200

    add comment

--- srcs/requirements/nginx/tools/ranki_crt_key_gen.sh
+++ srcs/requirements/nginx/tools/ranki_crt_key_gen.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+# Stop si ya une erreur
 set -e
 
+# Vérifie si le certif SSL existe pas déjà
 if [ ! -f /etc/ssl/certs/nginx.crt ]; then
-openssl req -x509 -nodes -days 365 -newkey rsa:4096 -keyout /etc/ssl/private/nginx.key -out /etc/ssl/certs/nginx.crt -subj "/C=KR/ST=Seoul/L=Seoul/O=${WP_TITLE}/CN=${WP_URL}";
+  # Genère un certif SSL auto-signé si il existe pas
+  openssl req -x509 -nodes -days 365 -newkey rsa:4096 -keyout /etc/ssl/private/nginx.key -out /etc/ssl/certs/nginx.crt -subj "/C=KR/ST=Seoul/L=Seoul/O=${WP_TITLE}/CN=${WP_URL}";
 fi
 
+# Lance Nginx en mode non-daemon
 exec nginx -g 'daemon off;'
